6ff40d79c6722b71f9be94fa172ed3f0327aecf4
Gintautas Petkevicius <g.petkevicius@inbox.lt>
2021-04-06 13:33:58 +0300

    redagavimas ir pasiruošimas šalinimui

--- src/main/java/ernadas_miestai/MiestaiController.java
+++ src/main/java/ernadas_miestai/MiestaiController.java
@@ -1,14 +1,18 @@
 package ernadas_miestai;
 
+import java.io.IOException;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
@@ -67,4 +71,53 @@ public class MiestaiController {
    
 		return "miestai";	
 	}	
+	
+	@RequestMapping(path="/miestas")	
+	public @ResponseBody Miestas miestoDuom(@RequestParam(name="id", required=true, defaultValue="0") Integer id ) throws IOException {
+		
+		String res;
+		
+		Miestas miestas = new Miestas();
+		
+		if (id > 0) {
+			
+			Optional <Miestas> found = miestas_repository.findById( id );
+		
+			// variantas trynimuiui
+			// uzsakymaiRepository.deleteById(id);
+		
+			if ( found.isPresent() ) {
+			
+			   miestas = found.get();
+			   miestas.setId ( id );
+			   
+			} else {
+				
+				res = "Klaida įrašas galėjoi būti pašalintas";
+			}
+		}		
+		
+		return miestas;
+	}
+	
+	/*
+	@GetMapping(path="/salinti-tiekeja") // Map ONLY GET Requests
+	public @ResponseBody String salintiTiekeja (@RequestParam Integer id 
+			) {
+		// @ResponseBody means the returned String is the response, not a view name
+		// @RequestParam means it is a parameter from the GET or POST request
+		
+		Optional <Tiekejai> found = tiekejaiRepository.findById( id );
+		
+		String res = "Not done";
+		
+		if ( found.isPresent() ) {
+			
+			   Tiekejai n = found.get();
+			   tiekejaiRepository.deleteById(id);
+			   res = "Deleted";
+		}		
+		return res;
+	}
+	*/	
 }
