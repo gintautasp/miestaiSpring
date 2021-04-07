21e67da3df88ea8b8e9fde462ba298ad98be72d6
Gintautas Petkevicius <g.petkevicius@inbox.lt>
2021-04-07 13:34:24 +0300

    One To Many sąryšis

--- src/main/java/ernadas_miestai/Salis.java
+++ src/main/java/ernadas_miestai/Salis.java
@@ -1,11 +1,15 @@
 package ernadas_miestai;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 
 import java.io.Serializable;
+import java.util.List;
 
 @Entity
 public class Salis implements Serializable {
@@ -17,6 +21,10 @@ public class Salis implements Serializable {
     private String salis;	
     
     private String salies_kodas;
+    
+    @OneToMany(/*mappedBy = "Miestas",*/cascade = CascadeType.ALL)
+    @JoinColumn(name="salies_kodas", referencedColumnName="salies_kodas", insertable=false, updatable=false)    
+    private List<Miestas> miestas;     
 
 	public Integer getId() {
 		return Id;
@@ -40,6 +48,14 @@ public class Salis implements Serializable {
 
 	public void setSalies_kodas(String salies_kodas) {
 		this.salies_kodas = salies_kodas;
+	}
+
+	public List<Miestas> getMiestas() {
+		return miestas;
+	}
+
+	public void setMiestas(List<Miestas> miestas) {
+		this.miestas = miestas;
 	}    
     
     
